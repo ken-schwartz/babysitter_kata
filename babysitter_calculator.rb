db8cc597e78edd40356c0fdab9bf75322df7a478
Jessica Huynh <jessica.huynh@beambenefits.com>
2025-05-22 13:22:10 -0700

    feat: added pay after bedtime

--- babysitter_calculator.rb
+++ babysitter_calculator.rb
@@ -1,7 +1,17 @@
 class BabysitterCalculator
-    def initialize(start_time, end_of_job, bed_time)
-    end
+  def initialize(start_time, end_of_job, bed_time) 
+    @start_time = start_time
+    @end_of_job = end_of_job
+    @bed_time = bed_time
+  end
 
-    def calculate_pay
-    end
+  def calculate_pay 
+    hours_worked_before_bedtime = @bed_time - @start_time 
+    hours_worked_after_bedtime = @end_of_job - @bed_time
+
+    pay_before_bedtime = hours_worked_before_bedtime * 12
+    pay_after_bedtime = hours_worked_after_bedtime * 8
+
+    total_pay = pay_before_bedtime + pay_after_bedtime
+  end
 end
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,12 +2,24 @@ require 'spec_helper'
 require_relative '../babysitter_calculator'
 
 RSpec.describe 'BabysitterCalculator' do
-  let(:start_time) { Time.new(2025, 5, 15, 17, 0, 0) }
-  let(:end_of_job) { Time.new(2025, 5, 15, 17, 0, 0) }
-  let(:bed_time) { Time.new(2025, 5, 15, 17, 0, 0) }
   let(:calculator) { BabysitterCalculator.new(start_time, end_of_job, bed_time) }
-  it 'pays $12 an hour from start time to bedtime ' do
-    value = calculator.calculate_pay
-    expect(value).to eq(12)
+  context 'when the babysitter works for 1 hour' do
+    let(:start_time) { 17 }
+    let(:end_of_job) { 18 }
+    let(:bed_time) { 18 }
+    it 'pays $12 an hour from start time to bedtime ' do
+      value = calculator.calculate_pay
+      expect(value).to eq(12)
+    end
+  end
+  
+  context 'when the babysitter works for 1 hour before bedtime and 1 hour after bedtime' do
+    let(:start_time) { 17 }
+    let(:end_of_job) { 19 }
+    let(:bed_time) { 18 }
+    it 'pays $12 an hour from start time to bedtime and $8 an hour after bedtime' do
+      value = calculator.calculate_pay
+      expect(value).to eq(20)
+    end
   end
 end
