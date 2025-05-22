003504b4c6637120b2a3b35149e09432a5f8573b
Jessica Huynh <jessica.huynh@beambenefits.com>
2025-05-22 16:18:44 -0700

    refactor: calculate_pay method

--- babysitter_calculator.rb
+++ babysitter_calculator.rb
@@ -6,12 +6,18 @@ class BabysitterCalculator
   end
 
   def calculate_pay 
+    pay_before_bedtime + pay_after_bedtime
+  end
+
+  private
+
+  def pay_before_bedtime
     hours_worked_before_bedtime = @bed_time - @start_time 
+    hours_worked_before_bedtime * 12
+  end
+
+  def pay_after_bedtime
     hours_worked_after_bedtime = @end_of_job - @bed_time
-
-    pay_before_bedtime = hours_worked_before_bedtime * 12
-    pay_after_bedtime = hours_worked_after_bedtime * 8
-
-    total_pay = pay_before_bedtime + pay_after_bedtime
+    hours_worked_after_bedtime * 8
   end
 end
